99ace7afa55af285503d0cbd9e53730350a1a864
PaulK <merveille.koffi@etu.unice.fr>
2020-05-13 13:19:11 +0200

    test

--- src/test/java/fr/unice/polytech/isa/dd/business/DeliveryBeanTest.java
+++ src/test/java/fr/unice/polytech/isa/dd/business/DeliveryBeanTest.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
-@RunWith(Arquillian.class)
+@RunWith(Arquillian.cla
 @Transactional(TransactionMode.COMMIT)
 public class DeliveryBeanTest extends AbstractDeliveryTest {
 
